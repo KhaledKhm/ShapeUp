ef861ac1831f496f4cf6f86cfdba292285bed0c9
KhaledKhm <47531479+KhaledKhm@users.noreply.github.com>
2020-05-02 23:31:10 +0100

    Add files via upload

---
+++ integration/views/logout.php
@@ -0,0 +1,24 @@
+<?php
+// On démarre la session
+session_start ();
+
+// On détruit les variables de notre session
+session_unset ();
+
+// On détruit notre session
+session_destroy ();
+
+// On redirige le visiteur vers la page d'accueil
+header ('location: auth.html');
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
