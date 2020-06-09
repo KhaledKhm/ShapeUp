654fa2677d80db976261dcfe9082e363bc469e83
nedrioussama <49002869+nedrioussama@users.noreply.github.com>
2020-06-09 06:30:36 +0100

    Add files via upload

--- integration/views/backreclamations.php
+++ integration/views/backreclamations.php
@@ -27,49 +27,6 @@ $listeReclamations=$reclamation1C->afficherReclamations();
 	https://templatemo.com/tm-524-product-admin
 	-->
 
-          <style>
-    #snackbar {
-  visibility: hidden;
-  min-width: 250px;
-  margin-left: -125px;
-  background-color: #333;
-  color: #fff;
-  text-align: center;
-  border-radius: 2px;
-  padding: 16px;
-  position: fixed;
-  z-index: 1;
-  left: 50%;
-  bottom: 30px;
-  font-size: 17px;
-}
-
-#snackbar.show {
-  visibility: visible;
-  -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
-  animation: fadein 0.5s, fadeout 0.5s 2.5s;
-}
-
-@-webkit-keyframes fadein {
-  from {bottom: 0; opacity: 0;} 
-  to {bottom: 30px; opacity: 1;}
-}
-
-@keyframes fadein {
-  from {bottom: 0; opacity: 0;}
-  to {bottom: 30px; opacity: 1;}
-}
-
-@-webkit-keyframes fadeout {
-  from {bottom: 30px; opacity: 1;} 
-  to {bottom: 0; opacity: 0;}
-}
-
-@keyframes fadeout {
-  from {bottom: 30px; opacity: 1;}
-  to {bottom: 0; opacity: 0;}
-}
-</style>
     </head>
 
   <body id="reportsPage">
@@ -268,7 +225,6 @@ $listeReclamations=$reclamation1C->afficherReclamations();
       </div>
     </div>
 
- <div id="snackbar">Bienvenue !</div>
 
 
 
